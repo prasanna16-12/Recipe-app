fb67f49b64c68a1e0f306edeec27dd9fa97d9712
prasanna16-12 <prasanna89kale@gmail.com>
2021-05-02 21:23:07 +0530

    Signed-off-by: prasanna16-12 <prasanna89kale@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import RecipeDetail from './RecipeDetail';
+import Home from './Home';
+
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Router>
+        <Route path='/' exact component={Home}></Route>
+        <Route path='/RecipeDetails/:obj' component={RecipeDetail}></Route>
+      </Router>
+    </React.Fragment>
   );
 }
 
